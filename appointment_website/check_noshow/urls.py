dc7f66eb3fabf939fcf36700b4d77dd3ba824dad
Czarek Polak <cezary.polak@dac.digital>
2022-03-10 17:27:47 +0100

    Working Form for a person

--- appointment_website/check_noshow/urls.py
+++ appointment_website/check_noshow/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='check-home'),
+    path('add_person', views.add_person, name='check-add_person'),
     ]
--- appointment_website/check_noshow/views.py
+++ appointment_website/check_noshow/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import PersonForm
 
 from datetime import datetime
 def home(request):
     return render(request, 'check_noshow/home.html',
                     {"cur_time": datetime.now()})
+
+def add_person(request):
+    if request.method == "POST":
+        form = PersonForm(request.POST)
+    else:
+        form = PersonForm()
+    return render(request, 'check_noshow/person.html',
+                        {"form": form})
