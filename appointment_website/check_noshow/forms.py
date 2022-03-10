a7327dfd63e95a1b20b5e4a828e1b5201acbd0d1
Czarek Polak <cezary.polak@dac.digital>
2022-03-10 22:58:34 +0100

    Working form

--- appointment_website/check_noshow/forms.py
+++ appointment_website/check_noshow/forms.py
@@ -1,7 +1,13 @@
 from .models import Person
-from django.forms import ModelForm
+from django.forms import ModelForm, RadioSelect, SelectDateWidget
 
 class PersonForm(ModelForm):
     class Meta:
         model = Person
         fields = '__all__'
+        # view in the form
+        widgets = {
+        'handicap' : RadioSelect(),
+        'date_of_appointment': SelectDateWidget(years=range(2020, 2040)),
+        'date_of_set_appointment' : SelectDateWidget(years=range(2020, 2040))
+        }
--- appointment_website/check_noshow/models.py
+++ appointment_website/check_noshow/models.py
@@ -1,6 +1,12 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from decimal import Decimal
+import uuid
+import datetime
+# import pretty
+
+def truncated_uuid4(uuid):
+    return str(uuid)[:5]
 
 # Create your models here.
 class Handicapchoices(models.IntegerChoices):
@@ -10,10 +16,12 @@ class Handicapchoices(models.IntegerChoices):
     FOURTH = 3, 'fourth'
     FIFTH = 4, 'fifth'
 class Person(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at =  models.DateTimeField(auto_now=True)
     date_of_appointment = models.DateTimeField()
     date_of_set_appointment = models.DateTimeField()
-    age = models.PositiveIntegerField()
+    age = models.PositiveIntegerField(validators=[MaxValueValidator(Decimal('102.0'))])
     scolarship = models.BooleanField()
     hipertension = models.BooleanField()
     diabetes = models.BooleanField()
@@ -22,5 +30,6 @@ class Person(models.Model):
     handicap = models.IntegerField(default=Handicapchoices.FIRST, choices=Handicapchoices.choices)
     num_app_missed = models.DecimalField(max_digits=3, decimal_places=0, validators=[MinValueValidator(Decimal('0.01'))])
 
+
     def __str__(self) -> str:
-        return f"{self.created_at}"
+        return f"Osoba o skróconym id: {truncated_uuid4(self.id)}, utworzona: {(self.created_at)}"
